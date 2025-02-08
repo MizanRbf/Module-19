34ecc590cc6684dd5dbcff4fa2c6fec6e9a0793c
MizanRbf <mizanrbf@gmail.com>
2025-02-08 22:34:58 +0600

    new data updated

--- loop.js
+++ loop.js
@@ -1,2 +1,52 @@
-const num = 1;
+// for 
 
+// const numbers = [1, 3, 4, 66, 33];
+// for (const num of numbers) {
+// console.log(num);
+// }
+
+// while
+
+// let num = 1;
+
+// while (num <= 10) {
+//   console.log(num);
+//   num++;
+// }
+
+// sum
+
+// let num = 1;
+// let sum = 0;
+// while (num <= 10) {
+//   console.log(num);
+//   sum = sum + num;
+//   console.log('sum:', sum);
+//   num++;
+// }
+
+// even
+
+// let num = 1;
+
+// while (num <= 10) {
+//   console.log(num);
+
+//   if(num % 2 === 0) {
+//     console.log('even number', num)
+//   }
+
+//   num++;
+// }
+
+
+// total sum
+
+let sum = 0;
+
+for (let i = 11; i <= 20; i++) {
+  sum = sum + i;
+  // console.log('sum:',sum);
+  console.log(i);
+}
+console.log('sum:',sum);
