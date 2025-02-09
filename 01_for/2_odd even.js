ffc53ed8b1c0d5df67a91a065e58c8ce01c299b6
MizanRbf <mizanrbf@gmail.com>
2025-02-09 09:34:51 +0600

    odd even task done

---
+++ 01_for/2_odd even.js
@@ -0,0 +1,27 @@
+// odd numbers
+for(let n = 61; n <= 100; n+=2) {
+  console.log(n);
+}
+
+// even numbers
+
+for(let n2 = 62; n2 <= 100; n2+=2) {
+  console.log(n2);
+}
+
+// odd numbers
+
+for(let n3 = 61; n3 <= 100; n3++) {
+  if(n3 % 2 === 1) {
+    console.log(n3);
+  }
+}
+
+// even numbers
+
+for(let n4 = 78; n4 <= 98; n4++) {
+  if(n4 % 2 === 0) {
+    console.log(n4);
+  }
+}
+
